c92c85f2b2dddad7570aa33714780591509e596b
N-Archer <archer.nj@gmail.com>
2021-04-27 01:06:34 +0000

    first commit

--- exercises/02-Select-DOM-Element/index.js
+++ exercises/02-Select-DOM-Element/index.js
@@ -1,1 +1,5 @@
 //your code here
+
+let aux = document.querySelector("#theTitle");
+//The variable aux now contains the H1 DOM element inside
+alert(aux.id);
--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -4,3 +4,9 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
+
+// var paragraph = document.createElement("p");
+// paragraph.style.background = "yellow";
+// paragraph.innerHTML = "Hello New World";
+document.querySelector("#myDiv").innerHTML += "<p>Hello New World</p>";
+document.querySelector("#p").innerHTML.style.background = "yellow";
