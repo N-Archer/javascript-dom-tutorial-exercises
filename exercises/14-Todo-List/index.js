59d92a76b5dcd4d3648a7d6e5ca52a1eefb544c2
N-Archer <archer.nj@gmail.com>
2021-04-30 23:47:41 +0000

    latest commit

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,26 @@
 // Your code here
+let tasks = ["Eat", "Drink", "Sleep"];
+
+let ul = document.querySelector("ul");
+let userInput = document.querySelector("#addToDo");
+let trash = document.querySelector("span");
+
+function deleteTask() {
+	let trash = document.querySelectorAll("span");
+	trash.forEach(span => {
+		span.addEventListener("click", e => span.parentNode.parentNode.removeChild(span.parentNode));
+	});
+}
+
+userInput.addEventListener("keypress", e => {
+	if (e.keyCode == 13 && e.target.value) {
+		ul.innerHTML += `<li>
+			<span>
+				<i class="fa fa-trash"></i>
+			</span>
+			${e.target.value}
+        </li>`;
+		userInput.value = null;
+		deleteTask();
+	}
+});
